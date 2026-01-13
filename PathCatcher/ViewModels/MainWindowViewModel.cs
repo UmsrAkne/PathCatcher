30aeeb934487d6cc6cfb81d6732fd327fcfbbd5e
UmsrAkne <sibata486@gmail.com>
2026-01-13 16:06:24 +0900

    feat: Add UI and functionality to add and display directory paths

--- PathCatcher/ViewModels/MainWindowViewModel.cs
+++ PathCatcher/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using PathCatcher.Utils;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using PathCatcher.Utils;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace PathCatcher.ViewModels;
@@ -6,6 +9,21 @@ namespace PathCatcher.ViewModels;
 public class MainWindowViewModel : BindableBase
 {
     private readonly AppVersionInfo appVersionInfo = new ();
+    private string pendingPath = string.Empty;
 
     public string Title => appVersionInfo.Title;
+
+    public ObservableCollection<string> DirectoryPaths { get; set; } = new ();
+
+    public string PendingPath { get => pendingPath; set => SetProperty(ref pendingPath, value); }
+
+    public DelegateCommand AddDPathCommand => new DelegateCommand(() =>
+    {
+        if (string.IsNullOrWhiteSpace(PendingPath) || !Directory.Exists(PendingPath))
+        {
+            return;
+        }
+
+        DirectoryPaths.Add(PendingPath);
+    });
 }
