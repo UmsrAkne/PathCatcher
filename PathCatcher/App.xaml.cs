6fc383a81948794d39b40d3d3e57bf3b40ba657d
UmsrAkne <sibata486@gmail.com>
2026-01-13 15:53:36 +0900

    init

---
+++ PathCatcher/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using PathCatcher.Views;
+using Prism.Ioc;
+
+namespace PathCatcher;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+}
---
+++ PathCatcher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PathCatcher.Utils;
+using Prism.Mvvm;
+
+namespace PathCatcher.ViewModels;
+
+public class MainWindowViewModel : BindableBase
+{
+    private readonly AppVersionInfo appVersionInfo = new ();
+
+    public string Title => appVersionInfo.Title;
+}
